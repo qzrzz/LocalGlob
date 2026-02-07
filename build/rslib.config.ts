13d0809f18f16233e4aa6b71f3879370b1eccd80
Qzrzz <qzrz256@gmail.com>
2026-02-07 15:57:15 +0800

    feat: Introduce a new Rslib project `aaaa` and update the root project's package metadata and build configurations.

--- build/rslib.config.ts
+++ build/rslib.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@rslib/core"
 export default defineConfig({
     source: {
         entry: {
-            index: "src/index.ts",
+            index: "./src/index.ts",
         },
     },
     lib: [
